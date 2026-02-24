15da700aaeb2f6a3dd0faddfa420e7386fe50b55
Niño Rollane Ocliasa <ocliasa@nctv360.com>
2026-02-24 17:26:26 +0800

    added public key

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://omqyynwejfpdblbzztvm.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tcXl5bndlamZwZGJsYnp6dHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4Mjk0OTMsImV4cCI6MjA4NzQwNTQ5M30.9Tu5E-iypHGoFRTgiV9k2RotJDvtmej8LkEtVjA-5IM',
+  }
+};
